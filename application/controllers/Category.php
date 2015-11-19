1447c3bfb34569acbfab7d8a79c8817c12b2b636
vihanga <vihangaliyanage007@gmail.com>
2015-11-19 10:46:11 +0530

    Add category form validation started

--- application/controllers/Category.php
+++ application/controllers/Category.php
@@ -9,21 +9,55 @@ class Category extends CI_Controller {
 		$this->load->model(array('category_model'));
 	}
 
-	public function index()
+	public function index(){
+		$data = array(
+			'cat_name' => "", 
+			'cat_image' => "",
+			'cat_nameErr' => "",
+			'cat_imageErr' => ""
+		);
+		$this->load->view('category_view', $data);
+	}
+
+	public function test(){
+		echo "test";
+	}
+
+	public function insert()
 	{
-		$cat_name = NULL;
-		$cat_image = NULL;
-		$add_category = NULL;
+		$isValid = array(False, False);
+
+		$cat_name = $cat_image = $add_category = $cat_nameErr = $cat_imageErr = "";
 
 		extract($_POST);
 
-		$params['cat_name'] = $cat_name;
-		$params['cat_image'] = $cat_image;
+		if (empty($cat_name)) {
+			$cat_nameErr = "Category name cannot be empty";
+			$isValid[0] = False;
+		} else {
+			$params['cat_name'] = $cat_name;
+			$isValid[0] = True;
+		}
 
-		if (isset($add_category)) {
+		if (empty($cat_image)) {
+			$cat_imageErr = "Category image cannot be empty";
+			$isValid[1] = False;
+		} else {
+			$params['cat_image'] = $cat_image;
+			$isValid[1] = True;
+		}
+		
+		if (isset($add_category) and (count(array_keys($isValid, True)) == count($isValid))) {
 			$this->category_model->insert($params);
 		}
 
-		$this->load->view('category_view');
+		$data = array(
+			'cat_name' => $cat_name, 
+			'cat_image' => $cat_image,
+			'cat_nameErr' => $cat_nameErr, 
+			'cat_imageErr' => $cat_imageErr
+		);
+
+		$this->load->view('category_view', $data);
 	}
 }
--- application/views/category_view.php
+++ application/views/category_view.php
@@ -6,9 +6,9 @@
 <body>
 	<h2>Welcome to category management!</h2>
 	<h3>Add new category</h3>
-	<form action="category" method="POST">
-		<input type="text" name="cat_name" placeholder="Category Name" />
-		<input type="text" name="cat_image" placeholder="Category Image" />
+	<form action="<?php echo site_url('category/insert');?>" method="POST">
+		<input type="text" name="cat_name" placeholder="Category Name" value="<?php echo $cat_name ?>" />
+		<input type="text" name="cat_image" placeholder="Category Image" value="<?php echo $cat_image ?>" />
 		<button type="submit" name="add_category" value="add">ADD</button>
 	</form>
 
