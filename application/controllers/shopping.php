0d9656718afead0eb615deedd5606fc200e42faf
vihanga <vihangaliyanage007@gmail.com>
2015-11-20 13:29:31 +0530

    File upload experimenting

---
+++ application/controllers/shopping.php
@@ -0,0 +1,17 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Shopping extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{	
+		$this->load->helper('url');
+		$this->load->view('home');
+	}
+}
+?>
+	
